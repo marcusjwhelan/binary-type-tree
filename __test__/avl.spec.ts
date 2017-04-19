38856adbfe4d1522a024bd6d884fb1dfa6361bfb
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-04-19 10:02:26 -0500

    add test framework

---
+++ __test__/avl.spec.ts
@@ -0,0 +1,7 @@
+import { AVLNode } from "../lib";
+
+test("basic node test", () => {
+    const node: AVLNode  = new AVLNode({key: "123", value: 3, unique: true});
+    expect(node).toBeInstanceOf(AVLNode);
+    expect(node.createRightChild({ key: "421", value: 4, unique: true})).toBeInstanceOf(AVLNode);
+});
---
+++ __test__/index.spec.ts
@@ -0,0 +1,7 @@
+import { sum } from "../lib";
+
+test("basic", () => {
+    expect(sum(1, 3)).toBe(4);
+});
+
+
